63e0e78be02359c73ca9715d0c4b18c8770fcae5
Li Yue Chen <22719973+l-yc@users.noreply.github.com>
2020-12-09 03:24:55 +0000

    add tools acknowledgements

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -1,6 +1,9 @@
 function MastHead() {
   return (
-    <div className="mast">A GeekOut Workshop Site</div>
+    <div className="mast">
+      A GeekOut Workshop Site
+      <p style={{color: 'white'}}>Made with Gitpod and Vercel</p>
+    </div>
   );
 }
 
